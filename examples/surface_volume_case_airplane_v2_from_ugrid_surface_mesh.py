ff9b27bf56b38474139305ca9bfae6aa685dc851
Feilin Jia <feilin@flexcompute.com>
2024-07-23 10:27:28 -0400

    restore solver version

--- examples/surface_volume_case_airplane_v2_from_ugrid_surface_mesh.py
+++ examples/surface_volume_case_airplane_v2_from_ugrid_surface_mesh.py
@@ -3,7 +3,7 @@ import os
 os.environ["FLOW360_BETA_FEATURES"] = "1"
 import flow360 as fl
 
-fl.Env.dev.active()
+fl.Env.preprod.active()
 from flow360.component.meshing.params import Farfield, Volume, VolumeMeshingParams
 from flow360.examples import Airplane
 
@@ -28,17 +28,17 @@ volume_mesh = fl.VolumeMesh.create(
     surface_mesh_id=surface_mesh.id,
     name="airplane-volume-mesh-from-ugrid",
     params=params,
-    solver_version="betaFL-24.7.3",
+    solver_version="mesher-24.2.2",
 )
 volume_mesh = volume_mesh.submit()
 
 # case
-#params = fl.Flow360Params(Airplane.case_json)
-#params.boundaries = {
-#    "farfield": fl.FreestreamBoundary(),
-#    "fuselage": fl.NoSlipWall(),
-#    "leftWing": fl.NoSlipWall(),
-#    "rightWing": fl.NoSlipWall(),
-#}
-#case_draft = volume_mesh.create_case("airplane-case-from-ugrid-surface-mesh-v2", params)
-#case = case_draft.submit()
+params = fl.Flow360Params(Airplane.case_json)
+params.boundaries = {
+    "farfield": fl.FreestreamBoundary(),
+    "fuselage": fl.NoSlipWall(),
+    "leftWing": fl.NoSlipWall(),
+    "rightWing": fl.NoSlipWall(),
+}
+case_draft = volume_mesh.create_case("airplane-case-from-ugrid-surface-mesh-v2", params)
+case = case_draft.submit()
